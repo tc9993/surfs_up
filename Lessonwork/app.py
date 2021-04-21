4e0f35f5c85b829a47248cf954a2d8beee4347d5
tc9993 <taylorbcoffee@gmail.com>
2021-04-21 15:50:51 -0500

    mod 9 lessonwork

--- Lessonwork/app.py
+++ Lessonwork/app.py
@@ -23,3 +23,57 @@ session = Session(engine)
 # set up flask
 app = Flask(__name__)
 
+# create welcome route
+@app.route('/')
+
+#create welcoem function w/ return statement
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:\n
+        /api/v1.0/precipitation\n
+        /api/v1.0/stations\n
+        /api/v1.0/tobs\n
+        /api/v1.0/temp/start/end
+        ''')
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route('/api/v1.0/tobs')
+def temp_monthly():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281') \
+        .filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps = temps)
+
+@app.route('/api/v1.0/temp/<start>')
+@app.route('/api/v1.0/temp/<start>/<end>')
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel) \
+            .filter(Measurement.date >= start) \
+            .filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+    
+    results = session.query(*sel) \
+        .filter(Measurement.date >= start) \
+        .filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)

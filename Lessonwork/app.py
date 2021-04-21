ce763de6cb6ec6bab8816364464eda6c9c3f3ff4
tc9993 <taylorbcoffee@gmail.com>
2021-04-20 21:01:02 -0500

    mod 9.5.2

--- Lessonwork/app.py
+++ Lessonwork/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
+# access sqlite database
+engine = create_engine('sqlite:///hawaii.sqlite')
+
+# reflect db into classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create session link to db
+session = Session(engine)
+
+# set up flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
